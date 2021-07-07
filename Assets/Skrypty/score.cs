75fbaaac4a04ad436ffb28b439968969214939f2
lShanterl <72656556+lShanterl@users.noreply.github.com>
2021-07-07 23:42:52 +0200

    scorus2

--- Assets/Skrypty/score.cs
+++ Assets/Skrypty/score.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class score : MonoBehaviour
 {
     private int currentScore;
-    public Text scoreText;
+    public TMP_Text scoreText;
     private void Update()
     {
         currentScore = 0;
@@ -15,16 +15,12 @@ public class score : MonoBehaviour
     {
         gameObject.tag = "scorus";
     }
-    private void HandleScore ()
-    {
-        scoreText.text = "Score:" + currentScore;
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag = "scorus");
+        if (collision.gameObject.tag == "scorus");
         {
-            currentScore = 1;
-            currentScore ++;
+            currentScore++;
+            scoreText.text = "Score:" + currentScore;
         }
     }
 }
