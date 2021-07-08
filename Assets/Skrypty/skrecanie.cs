ee50c7487171518de78c1639a490921bc3501e73
lis531 <lisekminecraftowy175@gmail.com>
2021-07-08 12:49:06 +0200

    Poprawki

--- Assets/Skrypty/skrecanie.cs
+++ Assets/Skrypty/skrecanie.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 public class skrecanie : MonoBehaviour
 {
+    private Vector2Int gridMoveDirection;
+    private Vector2Int gridPosition;
+    private float gridMoveTimer;
+    private float gridMoveTimerMax;
     GameObject apbuton;
     GameObject dawnbuton;
     GameObject rigtbuton;
     GameObject leftbuton;
     GameObject Snake;
+
     void Awake()
     {
+        gridPosition = new Vector2Int(0, 0);
+        gridMoveTimerMax = 1f;
+        gridMoveTimer = gridMoveTimerMax;
+        gridMoveDirection = new Vector2Int(1, 0);
         Snake = gameObject;
         apbuton = GameObject.Find("Button up");
         dawnbuton = GameObject.Find("Button down");
@@ -22,34 +31,47 @@ public class skrecanie : MonoBehaviour
     }
     public void TaskOnClickLeft()
     {
-        Snake.transform.rotation = Quaternion.Euler(0, 0, 90);
+        //Snake.transform.rotation = Quaternion.Euler(0, 0, 90);
         leftbuton.SetActive(false);
         rigtbuton.SetActive(false);
         apbuton.SetActive(true);
         dawnbuton.SetActive(true);
+        gridPosition.x -= 1;
     }
     public void TaskOnClickRight()
     {
-        Snake.transform.rotation = Quaternion.Euler(0, 0, -90);
+        //Snake.transform.rotation = Quaternion.Euler(0, 0, -90);
         leftbuton.SetActive(false);
         rigtbuton.SetActive(false);
         apbuton.SetActive(true);
         dawnbuton.SetActive(true);
+        gridPosition.x += 1;
     }
     public void TaskOnClickUp()
     {
-        Snake.transform.rotation = Quaternion.Euler(0, 0, 0);
+        //Snake.transform.rotation = Quaternion.Euler(0, 0, 0);
         apbuton.SetActive(false);
         dawnbuton.SetActive(false);
         leftbuton.SetActive(true);
         rigtbuton.SetActive(true);
+        gridPosition.y += 1;
     }
     public void TaskOnClickDown()
     {
-        Snake.transform.rotation = Quaternion.Euler(0, 0, 180);
+        //Snake.transform.rotation = Quaternion.Euler(0, 0, 180);
         apbuton.SetActive(false);
         dawnbuton.SetActive(false);
         leftbuton.SetActive(true);
         rigtbuton.SetActive(true);
+        gridPosition.y -= 1;
+    }
+
+    private void Update() {
+        gridMoveTimer += Time.deltaTime;
+        if (gridMoveTimer >= gridMoveTimerMax) {
+            gridPosition += gridMoveDirection;
+            gridMoveTimer -= gridMoveTimerMax;
+        }
+        transform.position = new Vector3(gridPosition.x, gridPosition.y);
     }
 }
