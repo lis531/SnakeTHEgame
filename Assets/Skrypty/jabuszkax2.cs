c31ef2ad7cc7b8794293290030cd03c73c7a2e66
lis531 <lisekminecraftowy175@gmail.com>
2021-10-13 22:01:26 +0200

    .

--- Assets/Skrypty/jabuszkax2.cs
+++ Assets/Skrypty/jabuszkax2.cs
@@ -6,33 +6,36 @@ public class jabuszkax2 : MonoBehaviour
 {
     score ScoreText;
     public BoxCollider2D pole;
-    GameObject jabuszkox2;
-
+    //GameObject jabuszkox2;
+    bool amogusTimer = false;
+    IEnumerator Amogus()
+    {
+        amogusTimer = false;
+        yield return new WaitForSeconds(10.0f);
+        amogusTimer = true;
+    }
     private void Start()
     {
-        jabuszkox2 = GameObject.Find("jabuszkozlote");
+        //jabuszkox2 = GameObject.Find("jabuszkozlote");
         ScoreText = GameObject.Find("Canvas/Score").GetComponent<score>();
+        StartCoroutine(Amogus());
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject.Find("Snake/glowa").GetComponent<TrailRenderer>().time += 1.0f;
         ScoreText.scoreAmount += 3;
-        jabuszkox2.SetActive(false);
-        RandomizePosition();
+        //jabuszkox2.SetActive(false);
 
+        if(amogusTimer)
+            RandomizePosition();
     }
     public void RandomizePosition()
     {
-        IEnumerator DelayAction(float delayTime)
-        {
-            yield return new WaitForSeconds(10);
-            jabuszkox2.SetActive(true);
             Bounds bounds = this.pole.bounds;
 
             float x = Random.Range(bounds.min.x, bounds.max.x);
             float y = Random.Range(bounds.min.y, bounds.max.y);
 
             this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
-        }
     }
 }
