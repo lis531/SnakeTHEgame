7e0f423a7770f73f636be645c65bafa54aaf3478
lis531 <lisekminecraftowy175@gmail.com>
2021-12-30 23:01:45 +0100

    .

--- Assets/Skrypty/Continue.cs
+++ Assets/Skrypty/Continue.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class Continue : MonoBehaviour
 {
     GameObject Snik;
-    GameObject Przycisk;
+    GameObject MenuButton;
+    GameObject RestartButton;
+    GameObject ContinueButton;
     void Start()
     {
         Snik = GameObject.Find("Snake");
-        Przycisk = GameObject.Find("Canvas/Continue");
-        transform.position = new Vector3(x: 1, y: 2, z: 3);
+        MenuButton = GameObject.Find("Canvas/Menu");
+        RestartButton = GameObject.Find("Canvas/Restart");
+        ContinueButton = GameObject.Find("Canvas/Continue");
     }
-    
+    public void ContinueButtonPressed()
+    {
+        ContinueButton.SetActive(false);
+        RestartButton.SetActive(false);
+        MenuButton.SetActive(false);
+        Snik.transform.position = new Vector3(0, 0, 0);
+    }
 }
--- Assets/Skrypty/kamienie.cs
+++ Assets/Skrypty/kamienie.cs
@@ -3,42 +3,26 @@ using UnityEngine;
 public class Kamienie : MonoBehaviour
 {
     public BoxCollider2D pole;
-    public float czas1;
-    public float czas2;
-    public float czas3;
-    public float czas4;
+    public float czas;
+    GameObject MenuButton;
+    GameObject RestartButton;
+    GameObject ContinueButton;
     IEnumerator Amugus()
     {
-        yield return new WaitForSeconds(czas1);
+        yield return new WaitForSeconds(czas);
         RandomizePosition();
     }
-    IEnumerator Amugu()
-    {
-        yield return new WaitForSeconds(czas2);
-        RandomizePositio();
-    }
-    IEnumerator Amug()
-    {
-        yield return new WaitForSeconds(czas3);
-        RandomizePositi();
-    }
-    IEnumerator Amu()
-    {
-        yield return new WaitForSeconds(czas4);
-        RandomizePosit();
-    }
-    IEnumerator Am()
-    {
-        yield return new WaitForSeconds(czas4);
-        RandomizePosi();
-    }
-
     void Start()
     {
+        //copy skalka object 5 times
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject kamien = Instantiate(gameObject);
+            kamien.transform.position = new Vector3(0, 0, 0);
+            kamien.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            kamien.transform.parent = transform;
+        }
         StartCoroutine(Amugus());
-        StartCoroutine(Amugu());
-        StartCoroutine(Amug());
-        StartCoroutine(Amu());
     }
     public void RandomizePosition()
     {
@@ -53,161 +37,14 @@ public class Kamienie : MonoBehaviour
         {
             RandomizePosition();
         }
-        if (gameObject.tag.Equals("skalka"))
-        {
-            RandomizePosition();
-        }
-        if (gameObject.tag.Equals("skalka1"))
-        {
-            RandomizePosition();
-        }
-        if (gameObject.tag.Equals("skalka2"))
-        {
-            RandomizePosition();
-        }
-        if (gameObject.tag.Equals("skalka3"))
-        {
-            RandomizePosition();
-        }
-        if (gameObject.tag.Equals("skalka4"))
-        {
-            RandomizePosition();
-        }
     }
-    public void RandomizePositio()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Bounds bounds = this.pole.bounds;
-
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-        float y = Random.Range(bounds.min.y, bounds.max.y);
-
-        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
-
-        if (gameObject.tag.Equals("skalka"))
+        if (other.gameObject.tag.Equals("skalka"))
         {
-            RandomizePositio();
-        }
-        if (gameObject.tag.Equals("skalka"))
-        {
-            RandomizePositio();
-        }
-        if (gameObject.tag.Equals("skalka1"))
-        {
-            RandomizePositio();
-        }
-        if (gameObject.tag.Equals("skalka2"))
-        {
-            RandomizePositio();
-        }
-        if (gameObject.tag.Equals("skalka3"))
-        {
-            RandomizePositio();
-        }
-        if (gameObject.tag.Equals("skalka4"))
-        {
-            RandomizePositio();
-        }
-    }
-    public void RandomizePositi()
-    {
-        Bounds bounds = this.pole.bounds;
-
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-        float y = Random.Range(bounds.min.y, bounds.max.y);
-
-        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
-
-        if (gameObject.tag.Equals("skalka"))
-        {
-            RandomizePositi();
-        }
-        if (gameObject.tag.Equals("skalka"))
-        {
-            RandomizePositi();
-        }
-        if (gameObject.tag.Equals("skalka1"))
-        {
-            RandomizePositi();
-        }
-        if (gameObject.tag.Equals("skalka2"))
-        {
-            RandomizePositi();
-        }
-        if (gameObject.tag.Equals("skalka3"))
-        {
-            RandomizePositi();
-        }
-        if (gameObject.tag.Equals("skalka4"))
-        {
-            RandomizePositi();
-        }
-    }
-    public void RandomizePosit()
-    {
-        Bounds bounds = this.pole.bounds;
-
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-        float y = Random.Range(bounds.min.y, bounds.max.y);
-
-        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
-
-        if (gameObject.tag.Equals("skalka"))
-        {
-            RandomizePosit();
-        }
-        if (gameObject.tag.Equals("skalka"))
-        {
-            RandomizePosit();
-        }
-        if (gameObject.tag.Equals("skalka1"))
-        {
-            RandomizePosit();
-        }
-        if (gameObject.tag.Equals("skalka2"))
-        {
-            RandomizePosit();
-        }
-        if (gameObject.tag.Equals("skalka3"))
-        {
-            RandomizePosit();
-        }
-        if (gameObject.tag.Equals("skalka4"))
-        {
-            RandomizePosit();
-        }
-    }
-    public void RandomizePosi()
-    {
-        Bounds bounds = this.pole.bounds;
-
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-        float y = Random.Range(bounds.min.y, bounds.max.y);
-
-        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
-
-        if (gameObject.tag.Equals("skalka"))
-        {
-            RandomizePosi();
-        }
-        if (gameObject.tag.Equals("skalka"))
-        {
-            RandomizePosi();
-        }
-        if (gameObject.tag.Equals("skalka1"))
-        {
-            RandomizePosi();
-        }
-        if (gameObject.tag.Equals("skalka2"))
-        {
-            RandomizePosi();
-        }
-        if (gameObject.tag.Equals("skalka3"))
-        {
-            RandomizePosi();
-        }
-        if (gameObject.tag.Equals("skalka4"))
-        {
-            RandomizePosi();
+            MenuButton.SetActive(true);
+            RestartButton.SetActive(true);
+            ContinueButton.SetActive(true);
         }
     }
 }
