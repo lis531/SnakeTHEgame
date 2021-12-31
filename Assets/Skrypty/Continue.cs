bc729182578b129855e5d3735e758fe774f252f2
lis531 <lisekminecraftowy175@gmail.com>
2021-12-31 16:09:39 +0100

    .

--- Assets/Skrypty/Continue.cs
+++ Assets/Skrypty/Continue.cs
@@ -1,25 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Continue : MonoBehaviour
-{
-    GameObject Snik;
-    GameObject MenuButton;
-    GameObject RestartButton;
-    GameObject ContinueButton;
-    void Start()
-    {
-        Snik = GameObject.Find("Snake");
-        MenuButton = GameObject.Find("Canvas/Menu");
-        RestartButton = GameObject.Find("Canvas/Restart");
-        ContinueButton = GameObject.Find("Canvas/Continue");
-    }
-    public void ContinueButtonPressed()
-    {
-        ContinueButton.SetActive(false);
-        RestartButton.SetActive(false);
-        MenuButton.SetActive(false);
-        Snik.transform.position = new Vector3(0, 0, 0);
-    }
-}
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//
+//public class Continue : MonoBehaviour
+//{
+//    GameObject Snik;
+//    GameObject MenuButton;
+//    GameObject RestartButton;
+//    GameObject ContinueButton;
+//    void Start()
+//    {
+//        Snik = GameObject.Find("Snake");
+//        MenuButton = GameObject.Find("Canvas/Menu");
+//        RestartButton = GameObject.Find("Canvas/Restart");
+//        ContinueButton = GameObject.Find("Canvas/Continue");
+//    }
+//    public void ContinueButtonPressed()
+//    {
+//        ContinueButton.SetActive(false);
+//        RestartButton.SetActive(false);
+//        MenuButton.SetActive(false);
+//        Snik.transform.position = new Vector3(0, 0, 0);
+//    }
+//}
